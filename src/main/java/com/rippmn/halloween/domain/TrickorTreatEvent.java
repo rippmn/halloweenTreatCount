caca7c570dc312efbe696419ea29580d1747ec6c
Andrew Ripka <rippmn@gmail.com>
2014-10-21 23:22:18 -0500

    add table name

--- src/main/java/com/rippmn/halloween/domain/TrickorTreatEvent.java
+++ src/main/java/com/rippmn/halloween/domain/TrickorTreatEvent.java
@@ -7,8 +7,10 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Table(name="tt_event")
 public class TrickorTreatEvent {
 
 	@Column
