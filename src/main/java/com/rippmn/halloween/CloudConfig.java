8882b6601ace034a08486df1b20ecc8745266304
Andrew Ripka <rippmn@gmail.com>
2014-12-04 09:51:24 -0600

    change name to match something containing mysql

--- src/main/java/com/rippmn/halloween/CloudConfig.java
+++ src/main/java/com/rippmn/halloween/CloudConfig.java
@@ -13,6 +13,6 @@ public class CloudConfig extends AbstractCloudConfig {
 
 	@Bean
     public DataSource getDataCloudSource(){    	
-		return connectionFactory().dataSource("halloweenUPSDB");
+		return connectionFactory().dataSource("halloween-mysql-UPSDB");
     }
 }
