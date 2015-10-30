02b99fa172e91783720af40b698feb34ff87cea9
Andrew Ripka <rippmn@gmail.com>
2015-10-30 10:32:40 -0500

    update repo

--- src/main/java/com/rippmn/halloween/persistence/TrickOrTreatEventRepository.java
+++ src/main/java/com/rippmn/halloween/persistence/TrickOrTreatEventRepository.java
@@ -8,6 +8,7 @@ import org.springframework.data.repository.CrudRepository;
 
 import com.rippmn.halloween.domain.TrickorTreatEvent;
 
+
 public interface TrickOrTreatEventRepository extends CrudRepository<TrickorTreatEvent, Long>{
 
 	@Query("select tt from TrickorTreatEvent tt where tt.eventDateTime >= ?")
@@ -16,5 +17,13 @@ public interface TrickOrTreatEventRepository extends CrudRepository<TrickorTreat
 	@Query("select tt from TrickorTreatEvent tt where tt.eventDateTime < CURRENT_DATE")
 	public List<TrickorTreatEvent> getPriorTTs();
 	
+	@Query("select tt from TrickorTreatEvent tt where YEAR(tt.eventDateTime) = ?")
+	public List<TrickorTreatEvent> getTtsByYear(Integer year);
 	
+	@Query("select MIN(tt.eventDateTime) from TrickorTreatEvent tt")
+	public Date getMinDate();
+	
+	@Query("select tt from TrickorTreatEvent tt where HOUR(tt.eventDateTime) = ?1 and (MINUTE(tt.eventDateTime) = ?2 or MINUTE(tt.eventDateTime) = ?3)")
+	public List<TrickorTreatEvent> getEventByTime(int hour, int minMinute, int maxMinute);
+		
 }
