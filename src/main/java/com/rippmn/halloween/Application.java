1172d645b0846814d282482d2a932423e123346b
Andrew Ripka <aripka@pivotal.io>
2016-10-31 22:55:57 -0500

    move enable scheduler to Service

--- src/main/java/com/rippmn/halloween/Application.java
+++ src/main/java/com/rippmn/halloween/Application.java
@@ -4,16 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @Configuration
 @ComponentScan
 @EnableAutoConfiguration
-@EnableScheduling
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
-    
+
 }
