4c73a26e777987a078ce49ac9642f3a4de724108
Andrew Ripka <aripka@Andrews-MacBook-Pro-2.local>
2017-10-09 22:26:10 -0500

    update to maven and SpringBoot 1.5.x

---
+++ src/test/java/com/rippmn/halloween/HalloweenApplicationTests.java
@@ -0,0 +1,16 @@
+package com.rippmn.halloween;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HalloweenApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
