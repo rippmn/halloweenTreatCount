affd21a2406c2a054161f4a35b796c577ab84777
rippmn <rippmn@gmail.com>
2020-10-31 08:39:07 -0500

    add a clock bean for use in new service

--- src/main/java/com/rippmn/halloween/Application.java
+++ src/main/java/com/rippmn/halloween/Application.java
@@ -1,13 +1,21 @@
 package com.rippmn.halloween;
 
+import java.time.Clock;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public Clock getClock(){
+        return Clock.systemDefaultZone();
     }
 
 }
